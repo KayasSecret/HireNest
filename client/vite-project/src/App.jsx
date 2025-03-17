7b9766697c2250bfbf68f48b975703083fb40b78
KayasSecret <kayasmishra29s@gmail.com>
2025-03-17 23:25:28 +0530

    #Comments-Clients

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import VisionVortex from "./components/VisionVortex.jsx";
-import JobDesk from "./components/JobDesk.jsx";
-import CodeClinic from "./components/CodeClinic.jsx";
-import "./index.css";
+import VisionVortex from "./components/VisionVortex.jsx"; // VisionVortex component ko import kar rahe hain
+import JobDesk from "./components/JobDesk.jsx"; // JobDesk component ko import kar rahe hain
+import CodeClinic from "./components/CodeClinic.jsx"; // CodeClinic component ko import kar rahe hain
+import "./index.css"; // CSS file import kar rahe hain
 
+// 🏠 Home component - Yahan se user different pages par navigate kar sakta hai
 function Home() {
   return (
     <div className="container">
@@ -18,15 +19,16 @@ function Home() {
   );
 }
 
+// 🌍 App component - Routes define kar raha hai
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/vision-vortex" element={<VisionVortex />} />
-      <Route path="/job-desk" element={<JobDesk />} />
-      <Route path="/code-clinic" element={<CodeClinic />} />
+      <Route path="/" element={<Home />} /> {/* Home page route */}
+      <Route path="/vision-vortex" element={<VisionVortex />} /> {/* Vision Vortex page route */}
+      <Route path="/job-desk" element={<JobDesk />} /> {/* Job Desk page route */}
+      <Route path="/code-clinic" element={<CodeClinic />} /> {/* Code Clinic page route */}
     </Routes>
   );
 }
 
-export default App
+export default App; // App component export kar rahe hain
