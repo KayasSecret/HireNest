3b471a5fb1a2c35277087e9f1ae5d9304477ebcc
KayasSecret <kayasmishra29s@gmail.com>
2025-03-17 22:13:47 +0530

    #Components

---
+++ vite-project/src/components/JobDesk.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function JobDesk() {
+  return (
+    <div className="container">
+      <h2>Job Desk</h2>
+      <div className="cards-container">
+        <div className="card">Card 1</div>
+        <div className="card">Card 2</div>
+        <div className="card">Card 3</div>
+        <div className="card">Card 4</div>
+      </div>
+    </div>
+  );
+}
+
+export default JobDesk;
